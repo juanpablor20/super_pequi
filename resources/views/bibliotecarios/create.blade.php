6f8d174bce91c1055825577a84d34d3c19ab36d0
juan pablo JPR <128744584+juanpablor20@users.noreply.github.com>
2024-06-10 19:16:29 -0500

    hola

--- resources/views/bibliotecarios/create.blade.php
+++ resources/views/bibliotecarios/create.blade.php
@@ -28,7 +28,7 @@
                                 <line x1="12" y1="5" x2="12" y2="19" />
                                 <line x1="5" y1="12" x2="19" y2="12" />
                             </svg>
-                           Lista de Bibliotecarios
+                           Lista de Bibliotecario
                         </a>
                     </div>
                 </div>
